36865ebc11953a235eff2d3ba3a6df0213976147
Exho1 <nat.adams149@gmail.com>
2015-06-30 12:18:56 -0500

    Created

---
+++ MassVMTUpdater/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MassVMTUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpenDialog = new System.Windows.Forms.Button();
+            this.dlgFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.tmrWaitForDirectory = new System.Windows.Forms.Timer(this.components);
+            this.txtPathDivider = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenDialog
+            // 
+            this.btnOpenDialog.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenDialog.Name = "btnOpenDialog";
+            this.btnOpenDialog.Size = new System.Drawing.Size(101, 28);
+            this.btnOpenDialog.TabIndex = 0;
+            this.btnOpenDialog.Text = "Select Directory";
+            this.btnOpenDialog.UseVisualStyleBackColor = true;
+            this.btnOpenDialog.Click += new System.EventHandler(this.btnOpenDialog_Click);
+            // 
+            // tmrWaitForDirectory
+            // 
+            this.tmrWaitForDirectory.Enabled = true;
+            this.tmrWaitForDirectory.Interval = 10;
+            this.tmrWaitForDirectory.Tick += new System.EventHandler(this.tmrWaitForDirectory_Tick);
+            // 
+            // txtPathDivider
+            // 
+            this.txtPathDivider.Location = new System.Drawing.Point(232, 28);
+            this.txtPathDivider.Name = "txtPathDivider";
+            this.txtPathDivider.Size = new System.Drawing.Size(120, 20);
+            this.txtPathDivider.TabIndex = 3;
+            this.txtPathDivider.Text = "/materials/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Top level folder:";
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(12, 123);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.ReadOnly = true;
+            this.txtDebug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDebug.Size = new System.Drawing.Size(419, 167);
+            this.txtDebug.TabIndex = 5;
+            this.txtDebug.Text = "Debug";
+            this.txtDebug.WordWrap = false;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 67);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(101, 28);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(232, 67);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 28);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 302);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPathDivider);
+            this.Controls.Add(this.btnOpenDialog);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mass VMT Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenDialog;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolderBrowser;
+        private System.Windows.Forms.Timer tmrWaitForDirectory;
+        private System.Windows.Forms.TextBox txtPathDivider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDebug;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ MassVMTUpdater/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MassVMTUpdater
+{
+    public partial class Form1 : Form
+    {
+
+        string[] folderContents;
+        int completedCount = 0;
+        int failedCount = 0;
+        int fileCount = 0;
+        string pathDivider;
+        string logPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            completedCount = 0;
+            failedCount = 0;
+        }
+
+        private void btnOpenDialog_Click(object sender, EventArgs e)
+        {
+            dlgFolderBrowser.ShowDialog();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            completedCount = 1;
+            failedCount = 0;
+            directoryIterate(folderContents);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tmrWaitForDirectory.Start();
+
+            dlgFolderBrowser.SelectedPath = "";
+            btnOpenDialog.Text = "Select Directory";
+
+            if (logPath != null)
+            {
+                File.WriteAllText(logPath, "");
+            }
+            txtDebug.Text = "Debug";
+        }
+
+        private void tmrWaitForDirectory_Tick(object sender, EventArgs e)
+        {
+            // Make sure a valid path is selected
+            // Hacky workaround because the folder browser doesnt have an event for when a folder is selected
+            if (dlgFolderBrowser.SelectedPath != "")
+            {
+                tmrWaitForDirectory.Stop();
+
+                btnOpenDialog.Text = "Selected!";
+
+                pathDivider = txtPathDivider.Text;
+                pathDivider = pathDivider.Replace("/", "");
+
+                folderContents = Directory.GetFiles(dlgFolderBrowser.SelectedPath, "*.vmt", SearchOption.AllDirectories);
+                fileCount = folderContents.GetUpperBound(0) + 1;
+
+                debugPrint("Found " + fileCount + " files.");
+                debugPrint(dlgFolderBrowser.SelectedPath);
+            }
+        }
+
+        public void directoryIterate( string[] contents )
+        {
+            // Get the location of the .exe file
+            string programLocation = System.Reflection.Assembly.GetEntryAssembly().Location;
+            int index = programLocation.LastIndexOf("\\") + 1;
+            programLocation = programLocation.Substring(0, index);
+
+            // Format it correctly
+            string backslash = "\\";
+            string forwardslash = "/";
+            programLocation = programLocation.Replace(backslash[0], forwardslash[0]);
+
+            logPath = programLocation + "log.txt";
+
+            // Create a log.txt file
+            File.WriteAllText(logPath, "Mass VMT Updater: " + Environment.NewLine);
+
+            // Edit each file
+            foreach( string path in contents )
+            {
+                if (File.Exists(path))
+                {
+                    logPrint("Editing " + path);
+                    editVMT( path );
+                }
+                else
+                {
+                    debugPrint("Invalid path: " + path);
+                    failedCount++;
+                }
+            }
+        }
+
+        // Modifies the VMT to update the $basetexture path to where the file currently is
+        public void editVMT(string path)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string vmtContents = "";
+
+                    int counter = 0;
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // Recreate the vmt's contents line by line
+                        if (vmtContents != "")
+                        {
+                            vmtContents = vmtContents + Environment.NewLine + line;
+                        }
+                        else
+                        {
+                            vmtContents = vmtContents + line;
+                        }
+
+                        string lowerLine = line.ToLower();
+                        if (lowerLine.Contains("$basetexture"))
+                        {
+                            // 13 = 12 characters for $basetexture + 1 because 0 indexing
+                            int index = lowerLine.IndexOf("$basetexture") + 13;
+                            string texturePath = line.Substring(index);
+                            texturePath = texturePath.Trim();
+
+                            // Divide the line where the basetexture path starts
+                            int dividerIndex = path.IndexOf(pathDivider) + pathDivider.Length;
+                            string newPath = path.Substring(dividerIndex);
+
+                            // Stupid hack to trim back slashes from the file path and replace them with forward slashes (when applicable)
+                            string backslash = "\\";
+                            string forwardslash = "/";
+                            newPath = newPath.TrimStart(backslash[0]);
+                            newPath = newPath.Replace(backslash[0], forwardslash[0]);
+
+                            // Make sure the path fits the VMT format
+                            newPath = '"' + newPath + '"';
+                            newPath = newPath.Replace(".vmt", "");
+
+                            // Replace the old path with our new one
+                            vmtContents = vmtContents.Replace(texturePath, newPath);
+
+                            debugPrint("Updated " + Path.GetFileName(path) + "! " + completedCount.ToString() + "/" + fileCount.ToString());
+                            completedCount++;
+
+                            if (fileCount < completedCount) {
+                                completedCount -= 1;
+
+                                debugPrint("----");
+                                debugPrint("Finshed updating VMT files!");
+                                debugPrint("Completed: " + completedCount.ToString());
+                                debugPrint("Failed: " + failedCount.ToString());
+                                debugPrint("----");
+                                break;
+                            }
+                        }
+                        counter++;
+                    }
+
+                    // Close stream reader so we can write
+                    sr.Close();
+
+                    // Write new contents
+                    File.WriteAllText(path, vmtContents);
+                    logPrint("Finished writing file (" + Path.GetFileName(path) + ")");
+                }
+            }
+            catch (Exception e)
+            {
+                debugPrint("The file (" + Path.GetFileName(path) + ") could not be read:");
+                debugPrint(e.Message);
+                failedCount++;
+            }
+        }
+
+        // Appends the message to the text box
+        public void debugPrint( string msg )
+        {
+            txtDebug.AppendText(Environment.NewLine + msg);
+
+            if (File.Exists(logPath))
+            {
+                File.AppendAllText(logPath, msg + Environment.NewLine);
+            }
+        }
+
+        public void logPrint(string msg)
+        {
+            if (File.Exists(logPath))
+            {
+                File.AppendAllText(logPath, msg + Environment.NewLine);
+            }
+        }
+    }
+}
